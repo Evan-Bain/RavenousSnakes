5579404cd977389c8ea44e0702cc8254babc3091
Evan-Bain <evanjbain@gmail.com>
2024-03-04 01:12:25 -0600

    Gradle migration and Nav Graph added

--- app/src/main/java/com/example/ravenoussnakes/stats/StatsFragment.java
+++ app/src/main/java/com/example/ravenoussnakes/stats/StatsFragment.java
@@ -1,4 +1,4 @@
-package com.example.ravenoussnakes;
+package com.example.ravenoussnakes.stats;
 
 import android.os.Bundle;
 
@@ -7,6 +7,8 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.example.ravenoussnakes.R;
 
 public class StatsFragment extends Fragment {
 
